6baaf4cb905c26d6f5a76ef679db0ca7f439fc51
Renato Pereira <renato.ppontes@gmail.com>
2014-10-11 00:14:57 -0300

    TMX Map added.

---
+++ src/tmx/TMXIsometricTileLayer.js
@@ -0,0 +1,102 @@
+/*
+* TMXIsometricTileLayer
+*
+* Copyright (c) 2014 Renato de Pontes Pereira.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+/**
+ * @module Creatine
+ **/
+
+// namespace:
+this.creatine = this.creatine || {};
+
+(function() {
+"use strict";
+
+/**
+ * TMXIsometricTileLayer represents a TileLayer in the TMX map.
+ *
+ * @class TMXIsometricTileLayer
+ * @param {creatine.TMXMap} map The map object.
+ * @param {Object} data The data object (from tmx format).
+ * @extends creatine.TMXTileLayer
+ * @constructor
+**/
+var TMXIsometricTileLayer = function(map, data) {
+    this.initialize(map, data);
+}
+var p = TMXIsometricTileLayer.prototype = new creatine.TMXTileLayer();
+
+    /**
+     * Create the tiles. Override this method on the child class.
+     * 
+     * @method _createTiles
+     * @protected
+    **/
+    p._createTiles = function() {
+        var order = this.map.renderOrder;
+        var startX = 0;
+        var startY = 0;
+        var endX = this.width;
+        var endY = this.height;
+        var dirX = 1;
+        var dirY = 1;
+        var tW = this.map.tileWidth;
+        var tH = this.map.tileHeight;
+        var offsetX = (tW/2)*(endX-1);
+        var offsetY = (tH);
+
+        if (order == 'left-down' || order=='left-up') {
+            startX = this.width-1; endX = 0; dirX = -1;
+        }
+        
+        if (order == 'right-up' || order=='left-up') {
+            startY = this.height-1; endY = 0; dirY = -1
+        }
+
+        for (var y=startY; y!=endY; y+=dirY) {
+            for (var x=startX; x!=endX; x+=dirX) {
+                var i = (this.width*y + x)
+
+                var gid = this.data[i];
+
+                if (gid <= 0) continue;
+
+                var tileset = this.map.getTilesetByGid(gid);
+                var tile = new createjs.Sprite(tileset.spritesheet)
+                var tileId = gid-tileset.firstgid;
+                
+                if (tileset.animations['anim-'+(tileId)]) {
+                    tile.gotoAndPlay('anim-'+(tileId));
+                } else {
+                    tile.gotoAndStop(tileId);
+                }
+
+                tile.x = (x - y)*tW/2 + offsetX;
+                tile.y = (x + y)*tH/2 + offsetY;
+                this.addChild(tile);
+            }
+        }
+    }
+
+creatine.TMXIsometricTileLayer = TMXIsometricTileLayer;
+}());
---
+++ src/tmx/TMXMap.js
@@ -0,0 +1,291 @@
+/*
+* TMXMap
+*
+* Copyright (c) 2014 Renato de Pontes Pereira.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+/**
+ * @module Creatine
+ **/
+
+// namespace:
+this.creatine = this.creatine || {};
+
+(function() {
+"use strict";
+
+/**
+ * TMXMap is the class that handle tile maps in the TMX format, specified by 
+ * the tile software Tiled. A TMX map is composed of one or more layers (tile 
+ * layers, object layers or image layers) and one or more tilesets (image 
+ * atlases). Creatine supports all types of layers and projections described by
+ * the TMX specification: orthogonal, isometric and staggered.
+ *
+ * The TMXMap receives a data object following the TMX specification. This is 
+ * usually loaded from a json map. 
+ *
+ * <h4>Example</h4>
+ *
+ * Notice that, TMXMap is a display object, inherited from 
+ * <code>createjs.Container</code>, it means that, after loading the map you
+ * can simply add it to the stage:
+ * 
+ *     var map = new creatine.TMXMap(dataObject);
+ *     stage.addChild(map);
+ *
+ * <h4>Data Format</h4>
+ *
+ * In relation to the map properties, the data must be in the following format:
+ * 
+ *     var data = {
+ *         'version'     : [Integer], // default to 1
+ *         'orientation' : [String],  // default to 'orthogonal'
+ *         'renderorder' : [String],  // default to 'right-down'
+ *         'height'      : [Integer], // default to 0
+ *         'width'       : [Integer], // default to 0
+ *         'tileheight'  : [Integer], // default to 0
+ *         'tilewidth'   : [Integer], // default to 0
+ *         'tilesets'    : [Array],   // mandatory
+ *         'layers'      : [Array],   // mandatory
+ *         'properties'  : [Object],
+ *     }
+ * 
+ * Consult the specific classes for layer and tileset to known more about the
+ * data format for these structures or consult the official TMX description:
+ * https://github.com/bjorn/tiled/wiki/TMX-Map-Format
+ *
+ * @class TMXMap
+ * @extends createjs.Container
+ * @param {Object} data A data object describing the map.
+ * @constructor
+**/
+var TMXMap = function(data) {
+    this.initialize(data);
+}
+var p = TMXMap.prototype = new createjs.Container();
+    
+    /**
+     * Map version
+     *
+     * @property version
+     * @type {Integer}
+     * @readonly
+    **/
+    p.version = null;
+
+    /**
+     * Orientation of the map, it supports "orthogonal", "isometric" and 
+     * "staggered".
+     *
+     * @property orientation
+     * @type {String}
+     * @readonly
+    **/
+    p.orientation = null;
+
+    /**
+     * Order of rendering, supports "right-down", "right-up", "left-down" and
+     * "left-up".
+     *
+     * @property renderOrder
+     * @type {String}
+     * @readonly
+    **/
+    p.renderOrder = null;
+
+    /**
+     * Describes how many tiles the map have in the y axis.
+     *
+     * @property height
+     * @type {Integer}
+     * @readonly
+    **/
+    p.height = null;
+
+    /**
+     * Describes how many tiles the map have in the x axis.
+     *
+     * @property width
+     * @type {Integer}
+     * @readonly
+    **/
+    p.width = null;
+
+    /**
+     * Height of tiles.
+     *
+     * @property tileHeight
+     * @type {Integer}
+     * @readonly
+    **/
+    p.tileHeight = null;
+
+    /**
+     * Width of tiles.
+     *
+     * @property tileWidth
+     * @type {Integer}
+     * @readonly
+    **/
+    p.tileWidth = null;
+
+    /**
+     * The list of tilesets in the map.
+     *
+     * @property tilesets
+     * @type {Array}
+     * @readonly
+    **/
+    p.tilesets = null;
+
+    /**
+     * The list of layers in the map.
+     *
+     * @property layers
+     * @type {Array}
+     * @readonly
+    **/
+    p.layers = null;
+
+    /**
+     * User defined properties object.
+     *
+     * @property properties
+     * @type {Object}
+     * @readonly
+    **/
+    p.properties = null;
+
+
+    p.__Container_initialize = p.initialize;
+    /**
+     * Initialization method.
+     * 
+     * @method initialize
+     * @param {Object} data A data object describing the map.
+     * @protected
+    **/
+    p.initialize = function(data) {
+        this.__Container_initialize();
+
+        this.version     = data['version'] || 1;
+        this.orientation = data['orientation'] || 'orthogonal';
+        this.renderOrder = data['renderorder'] || 'right-down';
+        this.height      = data['height'] || 0;
+        this.width       = data['width'] || 0;
+        this.tileHeight  = data['tileheight'] || 0;
+        this.tileWidth   = data['tilewidth'] || 0;
+        this.properties  = data['properties'];
+        this.tilesets    = [];
+        this.layers      = [];
+
+        this._createTilesets(data);
+        this._createLayers(data);
+
+    }
+
+    /**
+     * Create the tileset objects.
+     * 
+     * @method _createTilesets
+     * @param {Object} data An object following the TMX specification.
+     * @protected
+    **/
+    p._createTilesets = function(data) {
+        for (var i=0; i<data['tilesets'].length; i++) {
+            this.tilesets.push(
+                new creatine.TMXTileset(data['tilesets'][i])
+            )
+        }
+    }
+
+    /**
+     * Create the layer objects.
+     * 
+     * @method _createLayers
+     * @param {Object} data An object following the TMX specification.
+     * @protected
+    **/
+    p._createLayers = function(data) {
+        for (var i=0; i<data['layers'].length; i++) {
+            var layerType = data['layers'][i].type;
+
+            if (layerType == 'tilelayer') {
+                if (this.orientation == 'orthogonal')
+                    var Layer = creatine.TMXOrthogonalTileLayer
+
+                else if (this.orientation == 'isometric')
+                    var Layer = creatine.TMXIsometricTileLayer
+
+                else if (this.orientation == 'staggered')
+                    var Layer = creatine.TMXStaggeredTileLayer
+
+                else
+                    throw new Error('Unknown layer type: "'+layerType+'"');
+
+                var layer = new Layer(this, data['layers'][i]);
+                this.layers.push(layer);
+                this.addChild(layer);
+
+            } else if (layerType == 'objectgroup') {
+                this.layers.push(
+                    new creatine.TMXObjectLayer(this, data['layers'][i])
+                );
+                
+            } else if (layerType == 'imagelayer') {
+                var layer = new creatine.TMXImageLayer(this, data['layers'][i])
+                this.addChild(layer);
+                this.layers.push(layer);
+            }
+        }
+    }
+
+    /**
+     * Returns the tileset which holds the provided global ID.
+     * 
+     * @method getTilesetByGid
+     * @param {Integer} gid A global ID.
+    **/
+    p.getTilesetByGid = function(gid) {
+        for (var i=this.tilesets.length-1; i>=0; i--) {
+            if (this.tilesets[i].firstgid <= gid) {
+                return this.tilesets[i];
+            }
+        }
+    }
+
+    /**
+     * Returns a layer by name
+     * 
+     * @method getLayerByName
+     * @param {String} name Layer name.
+    **/
+    p.getLayerByName = function(name) {
+        for (var i=0; i<this.layers.length; i++) {
+            if (this.layers[i].name == name) {
+                return this.layers[i];
+            }
+        }
+    }
+
+
+creatine.TMXMap = TMXMap;
+}());
